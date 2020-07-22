0684474a41de3e7cd9c070a4ba48f0cfa5039416
Jaehyuk Cho <nakaa15@gmail.com>
2020-07-22 10:11:46 +0900

    two sum solved

---
+++ src/leetcode/easy/TwoSum.java
@@ -0,0 +1,21 @@
+package leetcode.easy;
+
+/**
+ * Created by jh.choi on 2020-07-21.
+ */
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int[] result = new int[2];
+	}
+
+	public int[] twoSum(int[] nums, int target) {
+		int x,y = 0;
+		for (int i = 0; i<nums.length; i++ ) {
+
+		}
+
+
+		return null;
+	}
+}
